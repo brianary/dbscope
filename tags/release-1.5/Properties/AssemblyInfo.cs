25a173530deb6b4a9bb6c6778bfb3a4cf1c0e127
brianary <brianary@d076ab21-1a2e-0410-8e30-2da678b99ef8>
2007-04-18 02:14:43 +0000

    made a copy

---
+++ tags/release-1.5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DBScope")]
+[assembly: AssemblyDescription("Browse tables and views, get statistical information about columns, and query directly though any .NET data provider.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("webcoder.info")]
+[assembly: AssemblyProduct("DBScope")]
+[assembly: AssemblyCopyright("Creative Commons Attribution-Share Alike 3.0 License http://creativecommons.org/licenses/by-sa/3.0/")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("283d1f15-62b4-49d8-8be9-ff553e45dda2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
